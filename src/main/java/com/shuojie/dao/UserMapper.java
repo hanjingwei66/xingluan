0ff1326ca9b7222db804ede89aeba14380f6368e
dzd666 <599952686@qq.com>
2019-08-21 14:35:09 +0800

    123

--- src/main/java/com/shuojie/dao/UserMapper.java
+++ src/main/java/com/shuojie/dao/UserMapper.java
@@ -8,21 +8,14 @@ package com.shuojie.dao;
 
 import com.shuojie.domain.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Map;
 
 @Mapper
+@Repository
 public interface UserMapper {
     List<User> selectUser(User user);
 
-    /**
-     * 根据用户查询用户信息
-     */
-    public Map<String,Object> queryInfoByUsername(String username);
-
-    /**
-     *插入一条数据
-     */
-    public void insertData(Map<String,String> data);
 }
