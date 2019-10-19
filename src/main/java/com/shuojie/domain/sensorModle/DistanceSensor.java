4f881ca1a715c8d247a1be76f7f8e13ecc7f9250
hanjingwei66 <824219116@qq.com>
2019-10-19 09:14:45 +0800

    add.gitignore

--- src/main/java/com/shuojie/domain/sensorModle/DistanceSensor.java
+++
@@ -1,8 +0,0 @@
-package com.shuojie.domain.sensorModle;
-
-import lombok.Data;
-
-@Data
-public class DistanceSensor extends BaseSensor {
-    private Double distance;//距离
-}
--- src/main/java/com/shuojie/service/UserMerberService.java
+++
@@ -1,9 +0,0 @@
-package com.shuojie.service;
-
-
-import com.shuojie.utils.vo.Result;
-
-
-public interface UserMerberService {
-    Result sendMsg(String PhoneNumbers);
-}
